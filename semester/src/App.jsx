2a144906a1295ade98e0db1b1cfa3b6c6a7cc8e2
So Ki Wai Grace <graceskw@connect.hku.hk>
2024-01-13 02:02:31 +0800

    refactor: delete spline div

--- semester/src/App.jsx
+++ semester/src/App.jsx
@@ -10,9 +10,7 @@ function App() {
 
   return (
     <>
-    <div className="spline" style={{width: "80vw", height:"80vh", border: "5px solid black", borderRadius: "5px"}} onMouseOver={()=>{document.getElementById("rotateIcon").style.visibility = "visible"}} onMouseLeave={()=>{document.getElementById("rotateIcon").style.visibility = "hidden"}}>
-      <Spline scene="https://prod.spline.design/C4UHylOL9n9Wax63/scene.splinecode" style={{width: "80vw", height:"80vh"}}/>
-    </div>
+    <Spline scene="https://prod.spline.design/C4UHylOL9n9Wax63/scene.splinecode" style={{width: "80vw", height:"80vh", border: "5px solid black", borderRadius: "5px"}} onMouseOver={()=>{document.getElementById("rotateIcon").style.visibility = "visible"}} onMouseLeave={()=>{document.getElementById("rotateIcon").style.visibility = "hidden"}}/>
     <div id="rotateIcon" style={{visibility: "hidden", display: "flex", justifyContent: "flex-end"}}>
       <img src={rotate} alt="rotateIcon" style={{width: "5vw"}}/>
       <img src={zoomOut} alt="zoomOutIcon" style={{width: "5vw"}}/>
