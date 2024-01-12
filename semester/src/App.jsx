07e1859725d33e6f4b1a78af8e963354ff6e09c1
So Ki Wai Grace <graceskw@connect.hku.hk>
2024-01-13 02:04:30 +0800

    Revert "refactor: delete spline div"
    This reverts commit 2a144906a1295ade98e0db1b1cfa3b6c6a7cc8e2.

--- semester/src/App.jsx
+++ semester/src/App.jsx
@@ -10,7 +10,9 @@ function App() {
 
   return (
     <>
-    <Spline scene="https://prod.spline.design/C4UHylOL9n9Wax63/scene.splinecode" style={{width: "80vw", height:"80vh", border: "5px solid black", borderRadius: "5px"}} onMouseOver={()=>{document.getElementById("rotateIcon").style.visibility = "visible"}} onMouseLeave={()=>{document.getElementById("rotateIcon").style.visibility = "hidden"}}/>
+    <div className="spline" style={{width: "80vw", height:"80vh", border: "5px solid black", borderRadius: "5px"}} onMouseOver={()=>{document.getElementById("rotateIcon").style.visibility = "visible"}} onMouseLeave={()=>{document.getElementById("rotateIcon").style.visibility = "hidden"}}>
+      <Spline scene="https://prod.spline.design/C4UHylOL9n9Wax63/scene.splinecode" style={{width: "80vw", height:"80vh"}}/>
+    </div>
     <div id="rotateIcon" style={{visibility: "hidden", display: "flex", justifyContent: "flex-end"}}>
       <img src={rotate} alt="rotateIcon" style={{width: "5vw"}}/>
       <img src={zoomOut} alt="zoomOutIcon" style={{width: "5vw"}}/>
